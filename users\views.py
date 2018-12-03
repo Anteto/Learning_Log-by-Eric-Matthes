df667d61bd42186f6af00839c7b891c19cc72b10
Anteto <43307734+Anteto@users.noreply.github.com>
2018-12-03 11:23:33 +0800

    Create users\views.py

---
+++ users\views.py
@@ -0,0 +1,26 @@
+from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from django.contrib.auth import logout, login, authenticate
+from django.contrib.auth.forms import UserCreationForm
+
+
+# Create your views here.
+def logout_view(request):
+    logout(request)
+    return HttpResponseRedirect(reverse('learning_logs:index'))
+
+
+def register(request):
+    if request.method != 'POST':
+        form = UserCreationForm()
+    else:
+        form = UserCreationForm(data=request.POST)
+        if form.is_valid():
+            new_user = form.save()
+            authenticated_user = authenticate(username=new_user.username, password=request.POST['password1'])
+            login(request, authenticated_user)
+            return HttpResponseRedirect(reverse('learning_logs:index'))
+        
+    content = {'form': form}
+    return render(request, 'users/register.html', content)
