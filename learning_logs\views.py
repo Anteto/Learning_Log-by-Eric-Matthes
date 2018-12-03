37e47d9f1c74b3994f4115a3e7776bff30cde3bb
Anteto <43307734+Anteto@users.noreply.github.com>
2018-12-03 11:09:50 +0800

    Create learning_logs\views.py

---
+++ learning_logs\views.py
@@ -0,0 +1,89 @@
+from django.shortcuts import render
+from .models import Topic, Entry
+from django.http import HttpResponseRedirect, Http404
+from django.urls import reverse
+from .forms import TopicForm, EntryForm
+from django.contrib.auth.decorators import login_required
+
+
+# Create your views here.
+def index(request):
+    return render(request, 'learning_logs/index.html')
+
+
+# 显示所有主题
+@login_required
+def topics(request):
+    topics = Topic.objects.filter(owner=request.user).order_by('date_added')
+    
+    context = {'topics': topics}
+    return render(request, 'learning_logs/topics.html', context)
+
+
+# 显示单个主题及其所有条目
+@login_required
+def topic(request, topic_id):
+    topic = Topic.objects.get(id=topic_id)
+    if topic.owner != request.user:
+        raise Http404
+
+    entries = topic.entry_set.order_by('-date_added')
+    
+    context = {'topic': topic, 'entries': entries}
+    return render(request, 'learning_logs/topic.html', context)
+
+
+@login_required
+def new_topic(request):
+    if request.method != 'POST':
+        form = TopicForm()
+    else:
+        form = TopicForm(request.POST)
+        # 检查数据是否有效，有效则存入数据库
+        if form.is_valid():
+            new_topic=form.save(commit=False)
+            new_topic.owner=request.user
+            new_topic.save()
+            # form.save()
+            # 重定向到页面topics
+            return HttpResponseRedirect(reverse('learning_logs:topics'))
+            
+    context = {'form': form}
+    return render(request, 'learning_logs/new_topic.html', context)
+
+
+@login_required
+def new_entry(request, topic_id):
+    topic = Topic.objects.get(id=topic_id)
+    if request.method != 'POST':
+        form = EntryForm()
+    else:
+        form = EntryForm(data=request.POST)
+        if form.is_valid():
+            new_entry = form.save(commit=False)
+            new_entry.topic = topic
+            new_entry.save()
+            return HttpResponseRedirect(reverse('learning_logs:topic', args=[topic_id]))
+            
+    context = {'topic': topic, 'form': form}
+    return render(request, 'learning_logs/new_entry.html', context)
+
+
+@login_required
+def edit_entry(request, entry_id):
+    entry = Entry.objects.get(id=entry_id)
+    topic = entry.topic
+    if topic.owner != request.user:
+        raise Http404
+
+    if request.method != 'POST':
+        # 将现有数据专入表格
+        form = EntryForm(instance=entry)
+    else:
+        form = EntryForm(instance=entry, data=request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('learning_logs:topic', args=[topic.id]))
+            
+    context = {'entry': entry, 'topic': topic, 'form': form}
+    return render(request, 'learning_logs/edit_entry.html', context)
